40924841f0683b8ecce20fbb41db52abb7c62531
andreaairplane <andreadrematos@gmail.com>
2024-02-17 08:28:30 -0500

    made and connected nav bar

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import HomePage from "./pages/HomePage/HomePage";
 import FirstPage from "./pages/FirstPage/FirstPage";
 import SecondPage from "./pages/SecondPage/SecondPage";
 import ThirdPage from "./pages/ThirdPage/ThirdPage";
+import NavBar from "./components/NavBar/NavBar";
 
 function App() {
   return (
     <BrowserRouter>
+      <NavBar />
       <Routes>
         <Route path='/' element={<HomePage />} />
         <Route path='/first' element={<FirstPage />} />
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -3,7 +3,10 @@ import { NavLink } from "react-router-dom";
 function NavBar() {
     return (
         <section className="nav-bar">
-
+            <NavLink to='/'>Home</NavLink>
+            <NavLink to='/first'>First</NavLink>
+            <NavLink to='/second'>Second</NavLink>
+            <NavLink to='/third'>Third</NavLink>
         </section>
     )
 }
